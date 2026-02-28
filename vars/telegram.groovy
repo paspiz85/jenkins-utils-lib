1332eb248f4c67f7059daf7e2650107ee85ebfff
Pasquale Pizzuti <paspiz85@users.noreply.github.com>
2026-02-28 15:36:35 +0100

    telegram

---
+++ vars/telegram.groovy
@@ -0,0 +1,40 @@
+
+def call(Map a = [:]) {
+    sendMessage(a)
+}
+
+def sendMessage(Map a = [:]) {
+  if (!a.message) {
+    error "[telegram] Parameter 'message' is required"
+  }
+  if (!a.chatId) {
+    error "[telegram] Parameter 'chatId' is required"
+  }
+  def credentialId = a.credentialId ?: env.TELEGRAM_BOT_TOKEN_ID
+  if (!credentialId) {
+    error "[telegram] Parameter 'credentialId' not provided and TELEGRAM_BOT_TOKEN_ID not set"
+  }
+  withCredentials([
+    string(credentialsId: credentialId, variable: 'TELEGRAM_BOT_TOKEN')
+  ]) {
+    withEnv([
+      "TELEGRAM_CHAT_ID=${a.chatId}",
+      "TELEGRAM_THREAD_ID=${a.threadId ?: ''}",
+      "TELEGRAM_MESSAGE=${a.message}",
+      "TELEGRAM_PARSE_MODE=${a.parseMode ?: ''}"
+    ]) {
+      sh '''
+        set -e
+        BASE_URL="https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage"
+        ARGS="-d chat_id=${TELEGRAM_CHAT_ID} --data-urlencode text=${TELEGRAM_MESSAGE}"
+        if [ -n "${TELEGRAM_THREAD_ID}" ]; then
+          ARGS="$ARGS -d message_thread_id=${TELEGRAM_THREAD_ID}"
+        fi
+        if [ -n "${TELEGRAM_PARSE_MODE}" ]; then
+          ARGS="$ARGS -d parse_mode=${TELEGRAM_PARSE_MODE}"
+        fi
+        curl -sS -f -X POST "$BASE_URL" $ARGS
+      '''
+    }
+  }
+}
